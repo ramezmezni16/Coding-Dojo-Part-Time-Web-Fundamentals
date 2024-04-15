5f8a5deaad5a8adbabd192132470765588251926
ramezmezni16 <ramez.nabila@gmail.com>
2024-04-15 18:41:27 +0100

    changes

---
+++ Web Fundamentals/JavaScript/Practice/DojoWeather/script.js
@@ -0,0 +1,14 @@
+var Cookie = document.querySelector(".cookie-time")
+function hide() {
+    Cookie.remove();
+}
+function DegChanger() {
+    document.getElementById("deg01").innerHTML = "75°";
+    document.getElementById("deg02").innerHTML = "65°";
+    document.getElementById("deg03").innerHTML = "80°";
+    document.getElementById("deg04").innerHTML = "66°";
+    document.getElementById("deg05").innerHTML = "69°";
+    document.getElementById("deg06").innerHTML = "61°";
+    document.getElementById("deg07").innerHTML = "78°";
+    document.getElementById("deg08").innerHTML = "70°";
+}
